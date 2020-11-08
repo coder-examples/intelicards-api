f73dea29beab37424dc19f91f0941dc9ea85683d
coder-examples <shahaayush265@gmail.com>
2020-11-08 13:03:17 +0530

    Make changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const cards = require('./cards.js')
+const cards = require('./cards.min.js')
 
 const app = express();
 app.use(cors());
